623dd89339c55b5acff9e7eb4532d9a90c2ae74c
uyjulian <uyjulian@gmail.com>
2019-07-31 15:38:47 -0500

    Implement Opus decoder

---
+++ external/config.h
@@ -0,0 +1,15 @@
+
+#pragma once
+
+#define HAVE_PROTOTYPES 1
+#define HAVE_STDARG_PROTOTYPES 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_TYPES_H 1
+#define SIZEOF_INT 4
+#define SIZEOF_LONG 4
+#define SIZEOF_SHORT 2
+#define STDC_HEADERS 1
+#define USE_ALLOCA 1
+#define OPUS_BUILD 1
